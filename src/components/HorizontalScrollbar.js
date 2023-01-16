701cd52a0ebaa81cd73f5acfb94ba90f19c85d22
Lazizjon <ln41128n@pace.edu>
2023-01-16 12:45:16 -0500

    Cleaned Up Code

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Box } from '@mui/material';
 
-const HorizontalScrollbar = () => {
+const HorizontalScrollbar = ({ data }) => {
   return (
-    <div>HorizontalScrollbar</div>
+    <div>
+        {data.map((item) => (
+            <Box
+                key={item.id || item}
+                itemId={item.id || item}
+                title={item.id || item}
+                m="0, 40px"
+            >
+                {item}
+            </Box>
+        )
+        )}
+    </div>
   )
 }
 
