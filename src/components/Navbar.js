4b44eb3fd97286e45af607406257b0acf7026862
Lazizjon <ln41128n@pace.edu>
2023-01-15 19:18:38 -0500

    Added components and pages folder with their files

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { Stack } from '@mui/system';
+
+import Logo from '../assets/images/Logo.png';
+
+const Navbar = () => {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
