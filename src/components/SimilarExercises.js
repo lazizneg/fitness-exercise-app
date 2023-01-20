c6edfe4a45c684afab1c4ea36a422d259694126b
Lazizjon <ln41128n@pace.edu>
2023-01-20 17:15:55 -0500

    Updated SimilarExercises.js

--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -2,19 +2,24 @@ import React from 'react'
 import { Box, Stack, Typography } from '@mui/material';
 
 import HorizontalScrollbar from './HorizontalScrollbar';
+import Loader from './Loader';
 
 
-const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
-  return (
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => (
     <Box sx={{ mt: { lg: '100px', xs: '0px' }}}>
-        <Typography>
-
+        <Typography sx={{ fontSize: { lg: '44px', xs: '25px' }, ml: '20px' }} fontWeight={700} color="#000" mb="33px">
+            Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Target Muscle</span> Exercises
         </Typography>
         <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
-        {targetMuscleExercises.length && <HorizontalScrollbar data={targetMuscleExercises} />}
+        {targetMuscleExercises.length !== 0 ? <HorizontalScrollbar data={targetMuscleExercises} /> : <Loader />}
+        </Stack>
+        <Typography sx={{ fontSize: { lg: '44px', xs: '25px' }, ml: '20px', mt: { lg: '100px', xs: '60px' } }} fontWeight={700} color="#000" mb="33px">
+            Similar <span style={{ color: '#FF2625', textTransform: 'capitalize' }}>Equipment</span> Exercises
+        </Typography>
+        <Stack direction="row" sx={{ p: 2, position: 'relative' }}>
+            {equipmentExercises.length !== 0 ? <HorizontalScrollbar data={equipmentExercises} /> : <Loader />}
         </Stack>
     </Box>
-  )
-}
+);
 
 export default SimilarExercises
