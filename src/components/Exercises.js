c4bb5a2df9321e6be818a3cfbe3b0e7a7512096c
Lazizjon <ln41128n@pace.edu>
2023-01-20 16:10:35 -0500

    Created Details.js

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -7,7 +7,23 @@ import ExerciseCard from './ExerciseCard';
 
 const Exercises = ({ exercises, setExercises, bodyPart }) => {
   const [currentPage, setCurrentPage] = useState(1);
-  const [exercisesPerPage] = 9;
+  const [exercisesPerPage] = useState(6);
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      let exercisesData = [];
+
+      if (bodyPart === 'all') {
+        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+      } else {
+        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+      }
+
+      setExercises(exercisesData);
+    };
+
+    fetchExercisesData();
+  }, [bodyPart]);
 
   const indexOfLastExercise = currentPage * exercisesPerPage;
   const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
@@ -20,21 +36,15 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
   }
 
   return (
-    <Box id="exercises"
-      sx={{mt: { lg: '110px' }}}
-      mt="50px"
-      p="20px"
-    >
-      <Typography variant="h3" mb="46px">
-        Showing Results
-      </Typography>
+    <Box id="exercises" sx={{ mt: { lg: '109px' } }} mt="50px" p="20px">
+      <Typography variant="h4" fontWeight="bold" sx={{ fontSize: { lg: '44px', xs: '30px' } }} mb="46px">Showing Results</Typography>
       <Stack direction="row" sx={{ gap: { lg: '107px', xs: '50px' } }} flexWrap="wrap" justifyContent="center">
-        {currentExercises.map((exercise, index) => (
-          <ExerciseCard key={index} exercise={exercise} />
+        {currentExercises.map((exercise, idx) => (
+          <ExerciseCard key={idx} exercise={exercise} />
         ))}
       </Stack>
       <Stack sx={{ mt: { lg: '114px', xs: '70px' } }} alignItems="center">
-        {currentExercises.length > 9 && (
+        {exercises.length > 9 && (
           <Pagination
             color="standard"
             shape="rounded"
@@ -47,7 +57,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
         )}
       </Stack>
     </Box>
-  )
-}
+  );
+};
 
-export default Exercises
+export default Exercises;
