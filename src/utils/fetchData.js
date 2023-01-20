f771581bf8f8c5ebc9f3e6efe634e0c40af41bcc
Lazizjon <ln41128n@pace.edu>
2023-01-20 12:51:13 -0500

    Created ExerciseCard.js & Fixed API Call

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -2,7 +2,7 @@ export const exerciseOptions = {
   method: 'GET',
   headers: {
       'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     },
   };
   
